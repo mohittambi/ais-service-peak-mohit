aa66814b466d32ee5cf11817e7227000e0980fe1
mohittambi <er.mohittambi@gmail.com>
2019-08-16 20:40:02 +0530

    Cloud Formation file changes Fruit CRUD files in raw format Serverless updated

--- handlers/index.js
+++ handlers/index.js
@@ -1,7 +1,46 @@
-'use strict';
+'use strict'
+const AWS = require('aws-sdk');
 
-function storeData() {
-  console.log("HELLO WORLD");
+AWS.config.update({ region: "eu-west-1" });
+
+exports.handler = async (event, context) => {
+  const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+  const documentClient = new AWS.DynamoDB.DocumentClient({ region: "eu-west-1" });
+
+  const params = {
+    TableName: "Fruits",
+    Key: {
+      id: "101"
+    }
+  }
+
+  try {
+    const data = await documentClient.get(params).promise();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+
+}
+
+let data = [{
+  id: 1,
+  name: "apple",
+  quantity: "10"
+},
+{
+  id: 2,
+  name: "orange",
+  quantity: "3"
+}];
+
+async function storeData(event) {
+  // console.log(event);
+  console.log("HELLO Mr");
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  };
 }
 
 module.exports = {
